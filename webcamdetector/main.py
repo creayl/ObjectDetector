8f3ac78b474a23c3a7cfa8b71751be24d39d81e8
creayl <nikoback93@gmail.com>
2021-10-08 12:12:40 +0200

    refactoring...

--- webcamdetector/main.py
+++ webcamdetector/main.py
@@ -12,17 +12,18 @@ thres = 0.5
 nms_threshold = 0.2
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+cap.set(3, 1280)
+cap.set(4, 720)
+cap.set(10, 150)
 
 classNames = []
-classFile = "coco.names"
+classFile = "../nets/coco.names"
 with open(classFile, "rt") as f:
     classNames = f.read().rstrip("\n").split("\n")
 
 print("avail. classes: ", classNames)
-configPath = "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
-weightsPath = "frozen_inference_graph.pb"
+configPath = "../nets/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "../nets/ssd_mobilenet_v3_large_frozen_inference_graph.pb"
 
 net = cv2.dnn_DetectionModel(weightsPath, configPath)
 net.setInputSize(320, 320)
@@ -59,7 +60,7 @@ while True:
         cv2.putText(
             img,
             str(round(confs[i] * 100, 2)),
-            (box[0] + 200, box[1] + 30),
+            (box[0] + 300, box[1] + 30),
             cv2.FONT_HERSHEY_COMPLEX,
             1,
             (0, 255, 0),
@@ -67,4 +68,11 @@ while True:
         )
 
     cv2.imshow("output", img)
-    cv2.waitKey(1)
+     # press 'q' with the output window focused to exit.
+    # waits 1 ms every loop to process key presses
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        cv2.destroyAllWindows()
+        break
+
+print("Done.")
