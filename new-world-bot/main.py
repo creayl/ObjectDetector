496105dbb69e4b3559a28a5de90c49f62616a9fe
creayl <nikoback93@gmail.com>
2021-10-16 16:07:36 +0200

    removed cascade clasifier as we get our pretrained model from tf2 now.., added simple-digit-recognition

--- new-world-bot/main.py
+++ new-world-bot/main.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 import numpy as np
+import tensorflow as tf
+
 import os
 from time import time
 from windowcapture import WindowCapture
@@ -13,7 +15,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 wincap = WindowCapture(None)
 
 # load the trained model
-cascade_tree = cv.CascadeClassifier("../cascade_classifier/cascade/cascade.xml")
+#cascade_tree = cv.CascadeClassifier("../cascade_classifier/cascade/cascade.xml")
 # load an empty Vision class
 vision_tree = Vision(None)
 
@@ -21,30 +23,66 @@ cv.namedWindow(
     "output", flags=cv.WINDOW_NORMAL | cv.WINDOW_KEEPRATIO | cv.WINDOW_GUI_EXPANDED
 )
 
-loop_time = time()
-while True:
+# Read the graph.
+with tf.compat.v1.gfile.FastGFile('frozen_inference_graph.pb', 'rb') as f:
+    graph_def = tf.compat.v1.GraphDef()
+    graph_def.ParseFromString(f.read())
 
-    # get an updated image of the game
-    screenshot = wincap.get_screenshot()
+with tf.compat.v1.Session() as sess:
+    # Restore session
+    sess.graph.as_default()
+    tf.import_graph_def(graph_def, name='')
 
-    # do object detection
-    rectangles = cascade_tree.detectMultiScale(screenshot)
-
-    # draw the detection results onto the original image
-    detection_image = vision_tree.draw_rectangles(screenshot, rectangles)
-
-    # display the images
-    cv.imshow("output", detection_image)
-
-    # debug the loop rate
-    print("FPS {}".format(1 / (time() - loop_time)))
     loop_time = time()
+    while True:
 
-    # press 'q' with the output window focused to exit.
-    # waits 1 ms every loop to process key presses
-    key = cv.waitKey(1)
-    if key == ord("q"):
-        cv.destroyAllWindows()
-        break
+        # get an updated image of the game
+        screenshot = wincap.get_screenshot()
 
-print("Done.")
+        # Read and preprocess an image.
+        rows = screenshot.shape[0]
+        cols = screenshot.shape[1]
+        inp = cv.resize(screenshot, (300, 300))
+        inp = inp[:, :, [2, 1, 0]]  # BGR2RGB
+
+        # Run the model
+        out = sess.run([sess.graph.get_tensor_by_name('num_detections:0'),
+                        sess.graph.get_tensor_by_name('detection_scores:0'),
+                        sess.graph.get_tensor_by_name('detection_boxes:0'),
+                        sess.graph.get_tensor_by_name('detection_classes:0')],
+                       feed_dict={'image_tensor:0': inp.reshape(1, inp.shape[0], inp.shape[1], 3)})
+
+        # Visualize detected bounding boxes.
+        num_detections = int(out[0][0])
+        for i in range(num_detections):
+            classId = int(out[3][0][i])
+            score = float(out[1][0][i])
+            bbox = [float(v) for v in out[2][0][i]]
+            if score > 0.3:
+                x = bbox[1] * cols
+                y = bbox[0] * rows
+                right = bbox[3] * cols
+                bottom = bbox[2] * rows
+                cv.rectangle(screenshot, (int(x), int(y)), (int(right), int(bottom)), (125, 255, 51), thickness=2)
+
+        # do object detection
+        #rectangles = cascade_tree.detectMultiScale(screenshot)
+
+        # draw the detection results onto the original image
+        #detection_image = vision_tree.draw_rectangles(screenshot, rectangles)
+
+        # display the images
+        cv.imshow("output", screenshot)
+
+        # debug the loop rate
+        print("FPS {}".format(1 / (time() - loop_time)))
+        loop_time = time()
+
+        # press 'q' with the output window focused to exit.
+        # waits 1 ms every loop to process key presses
+        key = cv.waitKey(1)
+        if key == ord("q"):
+            cv.destroyAllWindows()
+            break
+
+    print("Done.")
---
+++ simple-digit-recognition/README.md
@@ -0,0 +1,4 @@
+Simple Digit Recognition OCR in OpenCV Python
+=============================================
+This code is originally based on Abid Rahman K's answer in [Simple Digit Recognition OCR in OpenCV-Python](http://stackoverflow.com/questions/9413216/simple-digit-recognition-ocr-in-opencv-python/9620295).
+Several tiny modification has been made to adapt OpenCV's new API. (https://github.com/eyarng/sdr)
